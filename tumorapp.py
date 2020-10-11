d85bc6aa31333e403d848a650e6a746356cc5fc7
Kartik-Mishra99 <kartik876.km@gmail.com>
2020-10-11 19:35:15 +0530

    first commit

---
+++ tumorapp.py
@@ -0,0 +1,166 @@
+import sys
+import os
+import numpy as np
+import tensorflow as tf
+from PIL import Image,ImageOps
+
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+
+config = ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.5
+config.gpu_options.allow_growth = True
+session = InteractiveSession(config=config)
+from tensorflow.keras import Input
+from tensorflow.keras.models import Model, load_model, save_model
+from tensorflow.keras.layers import Input, Activation, BatchNormalization, Dropout, Lambda, Conv2D, Conv2DTranspose, MaxPooling2D, concatenate
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+from tensorflow.keras import backend as K
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+smooth=100
+im_width = 256
+im_height = 256
+EPOCHS = 150
+BATCH_SIZE = 32
+learning_rate = 1e-4
+
+def adjust_data(img,mask):
+    img = img / 255
+    mask = mask / 255
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
+    
+    return (img, mask)
+
+def dice_coef(y_true, y_pred):
+    y_truef=K.flatten(y_true)
+    y_predf=K.flatten(y_pred)
+    And=K.sum(y_truef* y_predf)
+    return((2* And + smooth) / (K.sum(y_truef) + K.sum(y_predf) + smooth))
+
+def dice_coef_loss(y_true, y_pred):
+    return -dice_coef(y_true, y_pred)
+
+def iou(y_true, y_pred):
+    intersection = K.sum(y_true * y_pred)
+    sum_ = K.sum(y_true + y_pred)
+    jac = (intersection + smooth) / (sum_ - intersection + smooth)
+    return jac
+
+def jac_distance(y_true, y_pred):
+    y_truef=K.flatten(y_true)
+    y_predf=K.flatten(y_pred)
+    return - iou(y_true, y_pred)
+
+def unet(input_size=(256,256,3)):
+    inputs = Input(input_size)
+    
+    conv1 = Conv2D(64, (3, 3), padding='same')(inputs)
+    bn1 = Activation('relu')(conv1)
+    conv1 = Conv2D(64, (3, 3), padding='same')(bn1)
+    bn1 = BatchNormalization(axis=3)(conv1)
+    bn1 = Activation('relu')(bn1)
+    pool1 = MaxPooling2D(pool_size=(2, 2))(bn1)
+
+    conv2 = Conv2D(128, (3, 3), padding='same')(pool1)
+    bn2 = Activation('relu')(conv2)
+    conv2 = Conv2D(128, (3, 3), padding='same')(bn2)
+    bn2 = BatchNormalization(axis=3)(conv2)
+    bn2 = Activation('relu')(bn2)
+    pool2 = MaxPooling2D(pool_size=(2, 2))(bn2)
+
+    conv3 = Conv2D(256, (3, 3), padding='same')(pool2)
+    bn3 = Activation('relu')(conv3)
+    conv3 = Conv2D(256, (3, 3), padding='same')(bn3)
+    bn3 = BatchNormalization(axis=3)(conv3)
+    bn3 = Activation('relu')(bn3)
+    pool3 = MaxPooling2D(pool_size=(2, 2))(bn3)
+
+    conv4 = Conv2D(512, (3, 3), padding='same')(pool3)
+    bn4 = Activation('relu')(conv4)
+    conv4 = Conv2D(512, (3, 3), padding='same')(bn4)
+    bn4 = BatchNormalization(axis=3)(conv4)
+    bn4 = Activation('relu')(bn4)
+    pool4 = MaxPooling2D(pool_size=(2, 2))(bn4)
+
+    conv5 = Conv2D(1024, (3, 3), padding='same')(pool4)
+    bn5 = Activation('relu')(conv5)
+    conv5 = Conv2D(1024, (3, 3), padding='same')(bn5)
+    bn5 = BatchNormalization(axis=3)(conv5)
+    bn5 = Activation('relu')(bn5)
+
+    up6 = concatenate([Conv2DTranspose(512, (2, 2), strides=(2, 2), padding='same')(bn5), conv4], axis=3)
+    conv6 = Conv2D(512, (3, 3), padding='same')(up6)
+    bn6 = Activation('relu')(conv6)
+    conv6 = Conv2D(512, (3, 3), padding='same')(bn6)
+    bn6 = BatchNormalization(axis=3)(conv6)
+    bn6 = Activation('relu')(bn6)
+
+    up7 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(bn6), conv3], axis=3)
+    conv7 = Conv2D(256, (3, 3), padding='same')(up7)
+    bn7 = Activation('relu')(conv7)
+    conv7 = Conv2D(256, (3, 3), padding='same')(bn7)
+    bn7 = BatchNormalization(axis=3)(conv7)
+    bn7 = Activation('relu')(bn7)
+
+    up8 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(bn7), conv2], axis=3)
+    conv8 = Conv2D(128, (3, 3), padding='same')(up8)
+    bn8 = Activation('relu')(conv8)
+    conv8 = Conv2D(128, (3, 3), padding='same')(bn8)
+    bn8 = BatchNormalization(axis=3)(conv8)
+    bn8 = Activation('relu')(bn8)
+
+    up9 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(bn8), conv1], axis=3)
+    conv9 = Conv2D(64, (3, 3), padding='same')(up9)
+    bn9 = Activation('relu')(conv9)
+    conv9 = Conv2D(64, (3, 3), padding='same')(bn9)
+    bn9 = BatchNormalization(axis=3)(conv9)
+    bn9 = Activation('relu')(bn9)
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(bn9)
+
+    return Model(inputs=[inputs], outputs=[conv10])
+
+model = unet(input_size=(im_height, im_width, 3))
+
+decay_rate = learning_rate / EPOCHS
+opt = Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=decay_rate, amsgrad=False)
+model.compile(optimizer=opt, loss=dice_coef_loss, metrics=["binary_accuracy", iou, dice_coef])
+model.load_weights("unetmodel.hdf5")
+
+
+import streamlit as st
+st.write("""
+         # Brain Tumor Segmentation using MRI Scans
+         """
+         )
+st.write("Brain Tumor Segmentation using U-Net Architecture.")
+st.write("If in case the predicted tumor mask is completely black that means that tumor is not detected in MRI scan image.")
+file = st.file_uploader("upload image", type=["jpg", "png"])
+
+def model_predict(image_data, model):
+    size = (256,256)
+    image = ImageOps.fit(image_data, size, Image.ANTIALIAS)
+    x = np.asarray(image)
+    x=x/255.0
+    x = np.expand_dims(x, axis=0)
+    preds = model.predict(x)
+    preds = np.squeeze(preds) 
+    return preds
+
+
+if file is None:
+    st.text("Please upload Brain MRI Scan")
+else:
+    st.write(f"Brain MRI Scan")
+    image = Image.open(file)
+    st.image(image, use_column_width=False,caption='Original MRI Scan Image')
+    st.write(f"U-Net Segmentation Result")
+    preds = model_predict(image, model)
+    st.image(preds,use_column_width=False,caption="Predicted Tumor Mask")  
+
+    
